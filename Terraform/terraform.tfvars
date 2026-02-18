91638b5cfcc426f56e067dbb8efef563f02a67e2
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-02-18 13:19:46 +0000

    Added variables for the main module, with inputs taken through .tfvars file

---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,36 @@
+# General infrastructure specifications
+infra_region   = "ap-south-1"
+project_prefix = "BoardGame_Backend"
+ssh_key_name   = "EC2_SSH_key"
+
+# External access ports
+external_access_ports = {
+	Jenkins   = 8080
+	Nexus     = 8081
+	SonarQube = 9000
+	SSH       = 22
+}
+
+# EC2 instance specifications
+ec2_instances = {
+	Ansible = {
+		name      = "Ansible"
+		type      = "t2.micro"
+		root_size = 24
+	}
+	Jenkins = {
+		name      = "Jenkins"
+		type      = "t2.micro"
+		root_size = 24
+	}
+	Nexus = {
+		name      = "Nexus"
+		type      = "t2.medium"
+		root_size = 24
+	}
+	SonarQube = {
+		name      = "SonarQube"
+		type      = "t2.medium"
+		root_size = 24
+	}
+}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,4 @@
+# General infrastructure specifications
 variable "infra_region" {
 	description = "AWS Region where the infrastrucure will be created."
 	type        = string
@@ -14,4 +15,59 @@ variable "ssh_key_name" {
 	description = "Name of the SSH key-pair to be used to access EC2 instances."
 	type        = string
 	default     = "EC2_SSH_key"
+}
+
+# Application access ports
+variable "external_access_ports" { # Should be replaced with user-input
+	description = "Ports used for accessing project resources externally."
+	type        = map()
+	default     = {
+		Jenkins   = 8080
+		Nexus     = 8081
+		SonarQube = 9000
+		SSH       = 22
+	}
+}
+
+variable "default_access_ports" { # DO NOT REPLACE WITH USER-INPUT
+	description = "Default ports used by the respective application for access. DO NOT REPLACE WITH USER-INPUT!"
+	type        = map()
+	default     = {
+		Jenkins   = 8080
+		Nexus     = 8081
+		SonarQube = 9000
+		SSH       = 22
+	}
+}
+
+# EC2 specifications
+variable "ec2_instances" {
+	description = "Specifications for creating the EC2 instances requried for the project."
+	type        = map(object({
+		name      = string
+		type      = string
+		root_size = number
+	}))
+	default = {
+		Ansible = {
+			name      = "Ansible"
+			type      = "t2.micro"
+			root_size = 24
+		}
+		Jenkins = {
+			name      = "Jenkins"
+			type      = "t2.micro"
+			root_size = 24
+		}
+		Nexus = {
+			name      = "Nexus"
+			type      = "t2.medium"
+			root_size = 24
+		}
+		SonarQube = {
+			name      = "SonarQube"
+			type      = "t2.medium"
+			root_size = 24
+		}
+	}
 }
