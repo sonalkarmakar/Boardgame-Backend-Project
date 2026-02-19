829685da555ecf3137b1d7e4746068e9a320e928
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-02-19 10:52:47 +0000

    Changed the variable name to explicitly reflect its use

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,18 +1,18 @@
 # Generating SSH key-pair
-resource "tls_private_key" "generated_ssh_key" {
+resource "tls_private_key" "ec2_ssh_key" {
 	algorithm = "RSA"
 	rsa_bits  = 2048
 }
 # Saving private key to file
 resource "local_file" "private_key" {
-	content         = tls_private_key.generated_ssh_key.private_key_pem
-	filename        = "${path.root}/.ssh/${var.project_prefix}-${var.ssh_key_name}.pem"
+	content         = tls_private_key.ec2_ssh_key.private_key_pem
+	filename        = "${path.root}/.ssh/${var.project_prefix}-${var.ec2_ssh_key_name}.pem"
 	file_permission = "0600"
 }
 # Saving public key to file
 resource "local_file" "public_key" {
-	content         = tls_private_key.generated_ssh_key.public_key_openssh
-	filename        = "${path.root}/.ssh/${var.project_prefix}-${var.ssh_key_name}.pem"
+	content         = tls_private_key.ec2_ssh_key.public_key_openssh
+	filename        = "${path.root}/.ssh/${var.project_prefix}-${var.ec2_ssh_key_name}.pem"
 	file_permission = "0644"
 }
 
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -11,7 +11,7 @@ variable "project_prefix" {
 	default     = "BoardGame_Backend"
 }
 
-variable "ssh_key_name" {
+variable "ec2_ssh_key_name" {
 	description = "Name of the SSH key-pair to be used to access EC2 instances."
 	type        = string
 	default     = "EC2_SSH_key"
