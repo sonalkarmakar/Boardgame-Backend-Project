4c5d6335356f11e37c8983424e071499d42cefc8
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-02-25 12:06:36 +0000

    Fixed bug of creating multiple Security Groups

--- Terraform/SecurityGroup/outputs.tf
+++ Terraform/SecurityGroup/outputs.tf
@@ -1,4 +1,4 @@
-output "group_id" {
+output "security_group_id" {
 	description = "ID of the Security Group."
-	value = aws_security_group.project_security_group.id
+	value       = aws_security_group.project_security_group.id
 }
