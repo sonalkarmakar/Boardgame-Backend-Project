8a0f749d1e4737b9f31ae7c88fd29e2e8164a72b
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-02-25 10:18:09 +0000

    Defined the EC2 module

--- Terraform/EC2/main.tf
+++ Terraform/EC2/main.tf
@@ -1,1 +1,16 @@
 # AMI ID: ami-019715e0d74f695be
+resource "aws_instance" "compute_instance" {
+	ami = "ami-019715e0d74f695be"
+	instance_type = var.instance_type
+	
+	root_block_device {
+		delete_on_termination = true
+		volume_type           = "gp3"
+		volume_size           = var.root_vol_size
+	}
+}
+
+resource "aws_key_pair" "instance_key" {
+	key_name = var.ssh_key_name
+	public_key = var.ssh_public_key
+}
---
+++ Terraform/EC2/variables.tf
@@ -0,0 +1,28 @@
+variable "instance_name" {
+	description = "Name of the EC2 instance shown in AWS Console."
+	type        = string
+	default     = "Project_Compute_Instance"
+}
+
+variable "instance_type" {
+	description = "Type AWS EC2 instance."
+	type        = string
+	default     = "t2.medium"
+}
+
+variable "root_vol_size" {
+	description = "Size of the root volume of the EC2 instance."
+	type        = number
+	default     = 24
+}
+
+variable "ssh_key_name" {
+	description = "Name of the SSH key used to access the EC2 instance."
+	type        = string
+	default     = "EC2_SSH_Key"
+}
+
+variable "ssh_public_key" {
+	description = "Public SSH key for accessing EC2 instance."
+	type        = string
+}
