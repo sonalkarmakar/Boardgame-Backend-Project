86c85f4fea45e3491b7727e9599b1dd4cf58cd0c
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-03-26 11:00:54 +0530

    Changed Security Group rule creation to new recommended resource

--- Terraform/Modules/SecurityGroup/main.tf
+++ Terraform/Modules/SecurityGroup/main.tf
@@ -15,23 +15,45 @@ resource "aws_security_group" "project_security_group" {
 }
 
 # Define Inbound Rules for Security Group
-resource "aws_security_group_rule" "inbound_access" {
+# resource "aws_security_group_rule" "inbound_access" {
+# 	for_each = var.inbound_access_port
+	
+# 	type              = "ingress"
+# 	from_port         = each.value
+# 	to_port           = each.value
+# 	protocol          = "tcp"
+# 	cidr_blocks       = [ "0.0.0.0/0" ]
+# 	security_group_id = aws_security_group.project_security_group.id
+# }
+resource "aws_vpc_security_group_ingress_rule" "inbound_access" {
 	for_each = var.inbound_access_port
 	
-	type              = "ingress"
 	from_port         = each.value
 	to_port           = each.value
-	protocol          = "tcp"
-	cidr_blocks       = [ "0.0.0.0/0" ]
+	ip_protocol       = "tcp"
+	cidr_ipv4         = "0.0.0.0/0"
 	security_group_id = aws_security_group.project_security_group.id
+
+	tags = {
+		Name = each.key
+	}
 }
 
 # Define Outbound Rule for Security Group
-resource "aws_security_group_rule" "outbound_access" {
-	type              = "egress"
-	from_port         = 0
-	to_port           = 0
-	protocol          = "-1"
-	cidr_blocks       = [ "0.0.0.0/0" ]
+# resource "aws_security_group_rule" "outbound_access" {
+# 	type              = "egress"
+# 	from_port         = 0
+# 	to_port           = 0
+# 	protocol          = "-1"
+# 	cidr_blocks       = [ "0.0.0.0/0" ]
+# 	security_group_id = aws_security_group.project_security_group.id
+# }
+resource "aws_vpc_security_group_egress_rule" "outbound_access" {
+	ip_protocol       = "-1"
+	cidr_ipv4         = "0.0.0.0/0"
 	security_group_id = aws_security_group.project_security_group.id
+
+	tags = {
+		Name = "AllAccess"
+	}
 }
