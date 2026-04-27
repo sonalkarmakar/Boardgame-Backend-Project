4f1c6f229ec2f144282dfbe70de9b39765a7ad90
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-04-27 18:31:20 +0530

    Removed unused module output

--- Terraform/Modules/EC2/outputs.tf
+++ Terraform/Modules/EC2/outputs.tf
@@ -2,8 +2,3 @@ output "public_ip" {
 	description = "Public IP address of the EC2 instance."
 	value       = aws_instance.compute_instance.public_ip
 }
-
-output "instance_id" {
-	description = "EC2 Instance ID."
-	value       = aws_instance.compute_instance.id
-}
